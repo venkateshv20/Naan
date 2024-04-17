13b0493be34d5a1fb91c34ef4ab0e0c2d8b5030c
venkateshv20 <venkateshmathi37@gmail.com>
2024-04-17 18:29:12 +0530

    seonf commit

---
+++ new.php
@@ -0,0 +1,22 @@
+<?php
+$name="venkat";
+$age=27;
+$phonenumber=9942290390;
+$name1="Rajesh";
+$age1=23;
+$phonenumber1=9047290390;
+$name2="Kannan";
+$age2=26;
+$phonenumber2=9578692270;
+// echo($name);
+echo("******************************************\n");
+echo("|     Name     |   Age   |  Phonenumber  |\n");        
+echo("******************************************\n");        
+echo("|"   . $name.     "|".$age.   "|".$phonenumber."|"."\n");        
+echo("******************************************\n");        
+echo("|"   . $name1.    "|".$age1.  "|".$phonenumber1."|"."\n");        
+echo("******************************************\n");        
+echo("|"   . $name2.    "|".$age2. "|".$phonenumber2."|"."\n");              
+echo("******************************************");        
+
+?>
